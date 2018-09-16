2625833de5d261a68695f58c5995701f8afe6169
estrangegit <etienne.estrangin@gmail.com>
2018-09-16 16:36:24 +0200

    4.26 Spring Inversion of Control

---
+++ src/com/luv2code/springdemo/HelloSpringApp.java
@@ -0,0 +1,20 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+    public static void main( String[] args ) {
+
+        // load the spring configuration class
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        
+        // retrieve a bean from the spring container
+        Coach theCoach = context.getBean( "myCoach", Coach.class );
+        
+        // call methods on the bean
+        System.out.println(theCoach.getDailyWorkout());
+        
+        // close the application context
+        context.close();
+    }
+}
